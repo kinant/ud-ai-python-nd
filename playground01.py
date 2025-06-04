97e28168ec03f44e27e370abff3ff0028790a7d7
Kinan T <kinan.t@gmail.com>
2025-06-04 15:55:36 -0600

    C02P05 Complete

--- playground01.py
+++ playground01.py
@@ -1,1 +1,14 @@
-print("0" + 5)
+egg_count = 0
+
+def buy_eggs():
+    egg_count += 12 # purchase a dozen eggs
+
+# buy_eggs()
+
+## This works fine
+word = "hello"
+
+def some_function():
+    print(word)
+
+some_function()
