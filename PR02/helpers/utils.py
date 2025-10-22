f1388462cb4003371b1c115c5650f602a631ad5d
Kinan T <kinan.t@gmail.com>
2025-10-22 15:36:19 -0600

    PR02 - Commenting Pt 1
    Finished commenting helpers

--- PR02/helpers/utils.py
+++ PR02/helpers/utils.py
@@ -9,19 +9,19 @@ from os import path, listdir
 NORMALIZING_MEAN = [0.485, 0.456, 0.406]
 NORMALIZING_STD = [0.229, 0.224, 0.225]
 
-# https://www.geeksforgeeks.org/python/how-to-check-if-python-package-is-installed/
 def is_pkg_installed(package: str):
     """
     Function to check if a package is installed
+    Reference used: https://www.geeksforgeeks.org/python/how-to-check-if-python-package-is-installed/
     :param package: name of the package to check
     :return: True if the package is installed, False otherwise
     """
     return importlib.util.find_spec(package) is not None
 
-def get_random_image_path(from_path):
+def get_random_image_path():
     """
-    Get a random image path from the flowers
-    Test Folder. I will use this function to
+    Get a random image path from the flowers' dataset.
+    I will use this function to
     use the predict function on any random image
     in that folder.
     """
@@ -33,13 +33,12 @@ def get_random_image_path(from_path):
     # get a random image
     # build the dir/path
     rand_dir = 'flowers/test/' + str(rand_folder_idx) + '/'
-    # rand_dir = path.join(from_path, str(rand_folder_idx))
 
     # get a random file from the dir
     # https://stackoverflow.com/questions/701402/best-way-to-choose-a-random-file-from-a-directory
     rand_img = random.choice(listdir(rand_dir))
 
-    # build fuild image path
+    # build fluid image path
     rand_img_path = rand_dir + rand_img
 
     return rand_img_path
--- PR02/helpers/visualization.py
+++ PR02/helpers/visualization.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torchvision.transforms.v2
+
 from .utils import NORMALIZING_STD, NORMALIZING_MEAN
 from PIL import Image
 from math import floor
 import torch
 
-def plot_transformed_images(image_paths, transform):
+def plot_transformed_images(
+        image_paths: list[str],
+        transform: torchvision.transforms.v2.Transform) -> None:
+    """
+    Function to plot original and transformed images
+    Used as reference: https://www.kaggle.com/code/tirendazacademy/cats-dogs-classification-with-pytorch
+    :param image_paths: list of image paths to plot
+    :param transform: the transformation applied to each image
+    """
     for image_path in image_paths:
         with Image.open(image_path) as img:
             fig, ax = plt.subplots(1, 2)
@@ -25,7 +35,20 @@ def plot_transformed_images(image_paths, transform):
             ax[1].axis("off")
 
 # https://docs.pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-def show_image(image, ax=None, title=None, normalize=True):
+def show_image(
+        image:torch.Tensor,
+        ax=None,
+        title: str=None,
+        normalize:bool=True) -> None:
+    """
+    Function to show an image
+    :param image: image to show
+    :param ax: axis
+    :param title: title to display
+    :param normalize: flag to normalize the image
+    """
+
+
     if ax is None:
         fig, ax = plt.subplots()
     image = image.numpy().transpose((1, 2, 0))
@@ -49,48 +72,57 @@ def show_image(image, ax=None, title=None, normalize=True):
 
     return ax
 
-def plot_loss_curves(training_results):
+def plot_loss_curves(results: dict) -> None:
+    """
+    Function to plot loss curves
+    Used as reference: https://www.kaggle.com/code/tirendazacademy/cats-dogs-classification-with-pytorch
+    :param results: training results dictionary
+    """
 
-    results = dict(list(training_results.items()))
+    # Get the loss values
+    train_loss = results['train_loss']
+    valid_loss = results['valid_loss']
 
-    # Get the loss values of the results dictionary (training and test)
-    loss = results['train_loss']
-    test_loss = results['valid_loss']
+    # Get the accuracy values
+    train_accuracy = results['train_acc']
+    valid_accuracy = results['valid_acc']
 
-    # Get the accuracy values of the results dictionary (training and test)
-    accuracy = results['train_acc']
-    test_accuracy = results['valid_acc']
-
-    # Figure out how many epochs there were
+    # Determine how many epochs
     epochs = range(len(results['train_loss']))
 
-    # Setup a plot
+    # Setup plot
     plt.figure(figsize=(15, 7))
 
-    # Plot loss
+    # Plot losses
     plt.subplot(1, 2, 1)
-    plt.plot(epochs, loss, label='train_loss')
-    plt.plot(epochs, test_loss, label='valid_loss')
+    plt.plot(epochs, train_loss, label='train_loss')
+    plt.plot(epochs, valid_loss, label='valid_loss')
     plt.title('Loss')
     plt.xlabel('Epochs')
     plt.legend()
 
-    # Plot accuracy
+    # Plot accuracies
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, accuracy, label='train_accuracy')
-    plt.plot(epochs, test_accuracy, label='valid_accuracy')
+    plt.plot(epochs, train_accuracy, label='train_accuracy')
+    plt.plot(epochs, valid_accuracy, label='valid_accuracy')
     plt.title('Accuracy')
     plt.xlabel('Epochs')
     plt.legend();
 
 # https://github.com/kinant/aipnd-project/blob/master/Image%20Classifier%20Project.ipynb
-def process_image(image_path):
-    """Process an image path into a PyTorch tensor"""
+def process_image(image_path: str) -> torch.Tensor:
+    """
+    Function to process image into a torch tensor
+    Used as reference:
+        https://github.com/kinant/aipnd-project/blob/master/Image%20Classifier%20Project.ipynb
+        This is actually my project submission from years ago
+
+    :param image_path: path of the image
+    :return: the image as a tensor
+    """
 
     # open the image
     image = Image.open(image_path)
-
-    # print("Original Image size: ", image.size)
 
     # first resize the images where the shortest side is 256 px
     width, height = image.size
@@ -115,13 +147,8 @@ def process_image(image_path):
         # resize larger side to 256 * ratio
         new_height = int(floor(ratio * size[1]))
 
-
-    # print("W: {}, H: {}".format(new_width, new_height))
-
     # resize the image
     image = image.resize((new_width, new_height))
-
-    # print("Resized Image (keep aspect ratio): ", image.size)
 
     # perform center crop
     # https://stackoverflow.com/questions/16646183/crop-an-image-in-the-centre-using-pil
@@ -134,17 +161,14 @@ def process_image(image_path):
     bottom = (height + new_height)/2
 
     image = image.crop((left, top, right, bottom))
-    # print("cropped image size: ", image.size)
 
     # convert encoded color channels and convert to floats (divide by 255)
     np_image = np.array(image) / 255
-    # print(np_image)
 
     # normalize
     np_image = (np_image - NORMALIZING_MEAN) / NORMALIZING_STD
 
     # finally, transpose
-    # print("shape 1: ", np_image.shape)
     np_image = np_image.transpose((2, 0, 1))
     # print("transposed shape: ", np_image.shape)
 
@@ -154,7 +178,16 @@ def process_image(image_path):
     # We have to convert to a tensor before we return it
     return torch.Tensor(np_image)
 
-def process_image_simple(image_path, transform):
+def process_image_simple(image_path: str,
+                         transform: torchvision.transforms.v2.Transform) -> torch.Tensor:
+    """
+    Simpler function to process image into a torch tensor
+    :param image_path: image path
+    :param transform: transformation to apply to image
+    :return: processed image as a torch tensor
+    """
+
+    # Open image and apply transform
     image = Image.open(image_path)
     image = transform(image)
 
